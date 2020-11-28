7dde70052044dfd0747f5e8d131c8b1cadf77cf7
Sitruunaaaa <63010871@kmitl.ac.th>
2020-11-28 19:55:28 +0700

    add debug case

--- AreaUnknowPolygon/Area.cpp
+++ AreaUnknowPolygon/Area.cpp
@@ -18,8 +18,13 @@ int main()
 	else if(p.checkPoint == 0)
 	{
 		area = fabs(0.5 * (p.sumDown - p.sumUp));
-		printf("\nPolygon Area is : %.2f", area);
+		printf("\nPolygon Area is : %.2f unit^2\n\n", area);
 	}
+	else if(p.checkPoint == 2)
+	{
+		printf("\nIt's a Straight line, not a polygonal pattern.\n\n");
+	}
+	else {}
 	
 
 	return 0;
@@ -35,35 +40,56 @@ Calculate calSum()
 	int sides = 0;
 	printf("Enter the number of sides of the polygon : ");
 	scanf_s("%d", &sides);
-	printf("\nEnter Coordinates of the polygon point\nFollow this example\n");
-	printf("1 1\n2 2\n3 3\n4 4\n");
-	printf("\nEnter your Coordinates\n");
-
-	
-	for (int i = 0; i < sides; i++)		//Input
+	if (sides>2)
 	{
-		scanf_s("%d %d", &x[i], &y[i]);
-		getchar();
-	}
+		printf("\nEnter Coordinates of the polygon point\nFollow this example\n");
+		printf("1 1\n2 2\n3 3\n4 4\n");
+		printf("\nEnter your Coordinates\n");
 
-	x[sides] = x[0];	// Add Coordinate
-	y[sides] = y[0];
 
-	for (int i = 0; i < sides; i++)		//CheckPoint
-	{
-		if (x[i] == x[i + 1] && y[i] == y[i + 1])
+		for (int i = 0; i < sides; i++)		//Input
 		{
-			p.checkPoint = 1;
+			scanf_s("%d %d", &x[i], &y[i]);
+			getchar();
 		}
+
+		x[sides] = x[0];	// Add Coordinate
+		y[sides] = y[0];
+
+		for (int i = 0; i < sides; i++)		//CheckPoint
+		{
+			if (x[i] == x[i + 1] && y[i] == y[i + 1])
+			{
+				p.checkPoint = 1;
+			}
+		}
+
+		for (int i = 0; i < sides; i++)
+		{
+			p.sumDown += x[i] * y[i + 1];	// MultipleDown
+			p.sumUp += y[i] * x[i + 1];		// MultipleUp
+		}
+
+		return p;
+	}
+	else if (sides == 1)
+	{
+		p.checkPoint == 1;
+		return p;
+
+	}
+	else if (sides == 2)
+	{
+		p.checkPoint = 2;
+		return p;
+	}
+	else
+	{
+		printf("\n\nError Please Try Again.\n\n");
+		p.checkPoint = 3;
+		return p;
 	}
 	
-	for (int i = 0; i < sides; i++)
-	{
-		p.sumDown += x[i] * y[i + 1];	// MultipleDown
-		p.sumUp += y[i] * x[i + 1];		// MultipleUp
-	}
-
-	return p;
 
 }
 
